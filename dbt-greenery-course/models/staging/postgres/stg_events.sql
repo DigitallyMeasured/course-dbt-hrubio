2125553b2c19a4e2fc9c373e1a7ec15aeac776cc
DigitallyMeasured <hector@digitallymeasured.io>
2021-12-05 17:40:02 -0700

    Update stg_events.sql

--- dbt-greenery-course/models/staging/postgres/stg_events.sql
+++ dbt-greenery-course/models/staging/postgres/stg_events.sql
@@ -10,6 +10,17 @@ SELECT
     session_id,
     user_id,
     page_url,
+    REPLACE(page_url,'https://greenary.com/product/','') AS product_id,
+    CASE 
+      WHEN page_url LIKE '%browse%' THEN 'Search Page' 
+      WHEN page_url = 'https://greenary.com' THEN 'Home Page' 
+      WHEN page_url = 'https://greenary.com/help' THEN 'Help Page' 
+      WHEN page_url = 'https://greenary.com/shipping' THEN 'Shipping Page'
+      WHEN page_url = 'https://greenary.com/signup' THEN 'Registration Page'
+      WHEN page_url = 'https://greenary.com/checkout' THEN 'Checkout Page'
+      WHEN page_url LIKE 'https://greenary.com/product%' THEN 'Product Page' 
+      ELSE page_url 
+    END page_name,
     created_at,
     event_type
 FROM {{ source('tutorial', 'events') }}
